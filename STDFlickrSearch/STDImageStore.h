58f81320bd137c5c3c6d72706cc1aeb8a56c36c0
alex <abastide@gmail.com>
2014-10-02 14:34:42 +0800

    - implementation of image store

---
+++ STDFlickrSearch/STDImageStore.h
@@ -0,0 +1,13 @@
+//
+//  STDImageStore.h
+//  STDFlickrSearch
+//
+//  Created by Alexis Bastide on 02/10/14.
+//  Copyright (c) 2014 Steed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STDImageStore : NSObject
+
+@end
---
+++ STDFlickrSearch/STDImageStore.m
@@ -0,0 +1,13 @@
+//
+//  STDImageStore.m
+//  STDFlickrSearch
+//
+//  Created by Alexis Bastide on 02/10/14.
+//  Copyright (c) 2014 Steed. All rights reserved.
+//
+
+#import "STDImageStore.h"
+
+@implementation STDImageStore
+
+@end
